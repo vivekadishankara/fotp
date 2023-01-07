022ceceb33470818bdedf9215ddc30d1382a76f9
vivek-naik-einfochips <vn52635@bose.com>
2023-01-07 10:34:32 +0530

    TASK-1: added documentation

--- application/comm/Request.py
+++ application/comm/Request.py
@@ -1,7 +1,13 @@
+"""
+Module to define the Request class and other related classes
+"""
 from dataclasses import dataclass
 
 
 class RequestArgs:
+    """
+    Arguments appearing in the order request
+    """
     REQUEST_TYPE = 'RequestType'
     ORDERID = 'OrderID'
     TOKEN = 'Token'
@@ -21,6 +27,9 @@ class RequestArgs:
 
 @dataclass(frozen=True)
 class Request:
+    """
+    Represents the Order request
+    """
     request_type: str
     order_id: int
     token: int
@@ -38,7 +47,12 @@ class Request:
     num_copies: int
 
     @staticmethod
-    def parse(request_string):
+    def parse(request_string: str) -> dict:
+        """
+        Parse and oder string that is accompanies an order file
+        :param request_string: string read from an order file
+        :return: dict
+        """
         request_list = request_string.split('|')
         request_dict = {}
         for entry in request_list:
@@ -48,5 +62,9 @@ class Request:
         return request_dict
 
     def look_for_order(self):
+        """
+        look for new orders in the 'requests' folder
+        :return:
+        """
         pass
 
